febdaa9afabeea90979ede839ad40d9399418ddd
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-18 16:45:37 -0300

    test: add test to component chart

---
+++ src/components/Chart.test.tsx
@@ -0,0 +1,25 @@
+import { expect, test } from 'vitest';
+import { render } from 'vitest-browser-react';
+import Chart from './Chart';
+
+
+test('should render Chart with correct data', async  () => {
+    const { getByText } = render(<Chart totalIncome={1000} totalExpense={500} />);
+    getByText('Income');
+    getByText('Expense');
+    
+    // @ts-ignore
+    await expect.element(getByText('Income')).toBeInTheDocument();
+    
+    // @ts-ignore
+    await expect.element(getByText('Expense')).toBeInTheDocument();
+});
+
+test('should handle zero values', async  () => {
+    const { getByText } = render(<Chart totalIncome={0} totalExpense={0} />);
+    
+    // @ts-ignore
+    await expect.element(getByText('No data available')).toBeInTheDocument();
+});
+
+
