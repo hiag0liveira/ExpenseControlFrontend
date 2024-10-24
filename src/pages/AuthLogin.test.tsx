9b2a161e44dc8cb7d46ac716bfcfadb93c842a68
LucasPetruci <lucaspetruci100@gmail.com>
2024-10-24 16:07:50 -0300

    Refactor: test file names and update login page test

--- src/pages/AuthLogin.test.tsx
+++ src/pages/AuthLogin.test.tsx
@@ -1,31 +1,32 @@
-import { render} from 'vitest-browser-react'
+import { render } from 'vitest-browser-react'
 import { expect, test } from 'vitest'
 import Auth from './Auth'
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 import { MemoryRouter } from 'react-router-dom'
 
-test('should display register page', async () => {
-  // Renderiza register page
+test('should display login page when URL contains ?mode=login', async () => {
   const { getByText, getByPlaceholder } = render(
     <Provider store={store}>
-      <MemoryRouter >
-        <Auth />
+      <MemoryRouter>
+        {/* Passando o valor de search como prop para simular ?mode=login */}
+        <Auth search="?mode=login" />
       </MemoryRouter>
     </Provider>
   )
-    // @ts-ignore
-    await expect.element(getByText('Registration')).toBeInTheDocument()
 
-    // @ts-ignore
+    //@ts-ignore
+    await expect.element(getByText('Login')).toBeInTheDocument()
+
+    //@ts-ignore
     await expect.element(getByPlaceholder('Email')).toBeInTheDocument()
 
-    // @ts-ignore
+    //@ts-ignore
     await expect.element(getByPlaceholder('Password')).toBeInTheDocument()
 
-    // @ts-ignore
+  // @ts-ignore
     await expect.element(getByText('Submit')).toBeInTheDocument()
 
     // @ts-ignore
-    await expect.element(getByText(' Already have an account ?')).toBeInTheDocument()
+    await expect.element(getByText("You don't have an account?")).toBeInTheDocument()
 })
